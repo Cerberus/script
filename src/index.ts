3f36b0a3d64748a254ebb4b9e4b2e5c7869d111a
Cerberus <shadow_sun0708@hotmail.com>
2020-09-10 19:28:20 +0700

    import from new file

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export const add = (x: number, y: number) => x + y
 export const remove = (x: number, y: number) => x - y
 
 export const multiply = (x: number, y: number) => x * y
+
+export * from './fn'
