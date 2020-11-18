d4904749a10596f7c772de035d001816f7cfb848
Cerberus <shadow_sun0708@hotmail.com>
2020-11-18 16:01:07 +0700

    disable globalMock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
 import { globalMock } from 'mock'
 
-globalMock()
+// globalMock()
 
 Date.now = jest.fn(() => 1587000000000)
 Math.random = jest.fn(() => 0.5)
--- src/user/__tests__/index.ts
+++ src/user/__tests__/index.ts
@@ -1,24 +1,6 @@
-import { globalMock } from 'mock'
-
 describe('index', () => {
-	afterEach(() => {
-		globalMock()
-	})
 	it('format name', async () => {
-		const { getName } = await import('..')
-
-		expect(await getName()).toEqual('Mr.fake')
-	})
-	it('format custom name', async () => {
-		jest.mock('../api', () => ({
-			fetchName: jest.fn().mockResolvedValue('fake 2'),
-		}))
-
-		const { getName } = await import('..')
-
-		expect(await getName()).toEqual('Mr.fake 2')
-	})
-	it('format name again', async () => {
+		jest.mock('../api')
 		const { getName } = await import('..')
 
 		expect(await getName()).toEqual('Mr.fake')
