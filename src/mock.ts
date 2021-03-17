84bf97e467c950fa33cb0f78bf43fe02f0ae059d
Cerberus <shadow_sun0708@hotmail.com>
2021-03-17 22:14:38 +0700

    format file

--- src/mock.ts
+++ src/mock.ts
@@ -6,8 +6,10 @@ export const mock = (absolutePath: string) => {
 	const relativePath = toRelativePath(absolutePath)
 	beforeEach(() => {
 		jest.resetModules()
-	// @ts-ignore for running yarn setup:cache faster
-		const mockPaths = (TREE as Record<string, string[]>)[shortPath(relativePath)]
+		// @ts-ignore for running yarn setup:cache faster
+		const mockPaths = (TREE as Record<string, string[]>)[
+			shortPath(relativePath)
+		]
 
 		mockPaths
 			.map(mockPath => {
